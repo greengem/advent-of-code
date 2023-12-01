e7bca76222350197108bf226303c5e3632e1cb5f
Green Gem <chris_waitt@me.com>
2023-12-01 15:07:01 +0000

    Initial commit

---
+++ app/2023/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function TwentyTwentyThree() {
+    return (
+        <>
+            <Link href='/2023/day-1/part-1'>Day 1 - Part 1</Link>
+            <Link href='/2023/day-1/part-2'>Day 1 - Part 2</Link>
+        </>
+    )
+}
