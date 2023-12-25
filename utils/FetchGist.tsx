c43487e3663bf776e67872b3e8baa9760c436c4f
Green Gem <chris_waitt@me.com>
2023-12-25 19:44:29 +0000

    use correct nextjs fetching for gist

--- utils/FetchGist.tsx
+++ utils/FetchGist.tsx
@@ -1,21 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import Highlight from './Highlight';
-
-interface FetchGistProps {
-    gistUrl: string;
+export default async function FetchGist({ gistUrl }: { gistUrl: string }) {
+    const res = await fetch(gistUrl);
+    const code = await res.text();
+    return code;
 }
-
-const FetchGist: React.FC<FetchGistProps> = ({ gistUrl }) => {
-    const [code, setCode] = useState<string>('');
-
-    useEffect(() => {
-        fetch(gistUrl)
-            .then(response => response.text())
-            .then(text => setCode(text))
-            .catch(error => console.error('Error fetching Gist:', error));
-    }, [gistUrl]);
-
-    return <Highlight code={code} />;
-};
-
-export default FetchGist;
