f6b74c6c28cc9872aef4bbccd9b47130f3a2d0ed
Green Gem <chris_waitt@me.com>
2023-12-25 19:44:43 +0000

    New Navbar

--- ui/Navbar/Navbar.tsx
+++ ui/Navbar/Navbar.tsx
@@ -1,14 +1,47 @@
-import Link from "next/link";
+'use client'
+import { useState } from "react";
+import { Link } from "@nextui-org/link";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from "@nextui-org/navbar";
 
-export default function Navbar() {
+const menuItems = [
+    { label: "Day 1: Trebuchet", href: "/2023/day-1" },
+    { label: "Day 2: Cube Conundrum", href: "/2023/day-2" },
+];
+
+export default function MainNavigation() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return(
-        <nav className="bg-zinc-900 text-zinc-200 flex justify-between px-5 items-center">
-            <p className="font-semibold text-lg">
-                <Link href="/">Advent Of Code Solutions</Link>
-            </p>
-            <ul className="flex gap-5">
-                <li><Link href="/2023" className="py-5 block">2023</Link></li>
-            </ul>
-        </nav>
-    )
+        <Navbar 
+            onMenuOpenChange={setIsMenuOpen}
+            maxWidth="full" 
+            className="bg-green-700 text-white mb-10"
+        >
+            <NavbarContent justify="start">
+                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+            </NavbarContent>
+
+            <NavbarContent justify="center">
+                <NavbarBrand>
+                    <Link className="font-bold text-white text-4xl drop-shadow-lg" href="/">Advent Of Code 2023</Link>
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent justify="end">
+                <NavbarItem>
+                    <Link className="text-white drop-shadow-md text-xl" href="https://github.com/greengem/advent-of-code">Github</Link>
+                </NavbarItem>
+            </NavbarContent>
+
+            <NavbarMenu>
+                {menuItems.map((item, index) => (
+                    <NavbarMenuItem key={index}>
+                        <Link className="w-full text-2xl" color="foreground" href={item.href}>
+                            {item.label}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </Navbar>
+    );
 }
