e85e9078feb08defa976356dce939197808aaa40
Green Gem <chris_waitt@me.com>
2023-12-25 19:43:35 +0000

    Add git clone snippet to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import {Snippet} from "@nextui-org/snippet";
 import Tree from '@/ui/Tree'
 
 export default function Home() {
   return (
-    <div className="flex-grow flex justify-center items-center p-5">
+    <div className="flex-grow flex flex-col justify-center items-center p-5 space-y-2">
       <Tree />
+      <Snippet size="lg">git clone https://github.com/greengem/advent-of-code</Snippet>
     </div>
   )
 }
